ebc87d91ed08a3eea22dccf83de8cb623db623f0
Pietro Di Bello <pierodibello@gmail.com>
2021-05-16 08:59:24 +0200

    add csharp template

---
+++ csharp/StringCalculatorKata.Tests/StringCalculatorTest.cs
@@ -0,0 +1,14 @@
+using FluentAssertions;
+using Xunit;
+
+namespace StringCalculatorKata.Tests
+{
+    public class StringCalculatorTest
+    {
+        [Fact]
+        public void ExampleTest()
+        {
+            true.Should().Be(false);
+        }
+    }
+}
---
+++ csharp/StringCalculatorKata/StringCalculator.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace StringCalculatorKata
+{
+    public static class StringCalculator
+    {
+        private const string Separator = ",";
+
+        public static int Add(string numbers)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
