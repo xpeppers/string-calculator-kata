4fbd88c23b7f47b19f488560929ed68965cf3831
Pierpaolo Simoncini <simoncinip@gmail.com>
2021-11-12 12:33:13 +0100

    InputContainsTwoNumber

--- csharp/StringCalculatorKata.Tests/StringCalculatorTest.cs
+++ csharp/StringCalculatorKata.Tests/StringCalculatorTest.cs
@@ -1,26 +1,33 @@
 using FluentAssertions;
+
 using Xunit;
 
 namespace StringCalculatorKata.Tests
 {
-    public class StringCalculatorTest
-    {
-        [Fact]
-        public void EmptyStringEqualsZero()
-        {
-            StringCalculator.Add("").Should().Be(0);
-        }
+	public class StringCalculatorTest
+	{
+		[Fact]
+		public void EmptyStringEqualsZero()
+		{
+			StringCalculator.Add("").Should().Be(0);
+		}
 
-        [Fact]
-        public void ZeroEqualsZero()
-        {
-            StringCalculator.Add("0").Should().Be(0);
-        }
+		[Fact]
+		public void ZeroEqualsZero()
+		{
+			StringCalculator.Add("0").Should().Be(0);
+		}
 
-        [Fact]
-        public void InputEqualsNumberIdentity()
-        {
-            StringCalculator.Add("1").Should().Be(1);
-        }
-    }
+		[Fact]
+		public void InputEqualsNumberIdentity()
+		{
+			StringCalculator.Add("1").Should().Be(1);
+		}
+
+		[Fact]
+		public void InputContainsTwoNumber()
+		{
+			StringCalculator.Add("1,2").Should().Be(3);
+		}
+	}
 }
--- csharp/StringCalculatorKata/StringCalculator.cs
+++ csharp/StringCalculatorKata/StringCalculator.cs
@@ -1,14 +1,18 @@
 using System;
 namespace StringCalculatorKata
 {
-    public static class StringCalculator
-    {
-        public static int Add(string numbers)
-        {
-            if (string.IsNullOrEmpty(numbers))
-                return 0;
-                
-            return Int32.Parse(numbers);
-        }
-    }
+	public static class StringCalculator
+	{
+		public static int Add(string numbers)
+		{
+			if (string.IsNullOrEmpty(numbers))
+				return 0;
+
+			string[] addends = numbers.Split(@",");
+			if (addends.Length == 2)
+				return Convert.ToInt32(addends[0]) + Convert.ToInt32(addends[1]);
+
+			return Int32.Parse(numbers);
+		}
+	}
 }
