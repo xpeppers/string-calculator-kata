f0750a8c9f02bdee2f3f1309cf9cb3c51c5678d4
Luigi Ferrarelli <luigi.ferrarelli@dodifferent.it>
2022-01-11 12:58:44 +0100

    refactor add method

--- java/src/main/java/com/xpeppers/Calculator.java
+++ java/src/main/java/com/xpeppers/Calculator.java
@@ -7,12 +7,15 @@ private static final String NUMBER_SEPARATOR=",";
     public static int add(String numbers){
         if (numbers.isEmpty())
             return 0;
-
         String[] number = numbers.split(NUMBER_SEPARATOR);
-        Integer sum = 0;
+        return sumNumbers(number);
+    }
+
+    private static Integer sumNumbers(String[] number) {
+        Integer result = 0;
         for (int i = 0; i< number.length; i++){
-            sum = sum + Integer.valueOf(number[i]);
+            result = result + Integer.valueOf(number[i]);
         }
-        return sum;
+        return result;
     }
 }
--- java/src/test/java/com/xpeppers/CalculatorTest.java
+++ java/src/test/java/com/xpeppers/CalculatorTest.java
@@ -13,14 +13,15 @@ public class CalculatorTest {
     }
 
     @Test
-    public void sumTwoNumbers(){
-        int result = Calculator.add("1,2");
-        assertEquals(3, result);
-    }
-    @Test
     public void testWithOneNumber(){
         int result = Calculator.add("1");
         assertEquals(1, result);
+    }
+
+    @Test
+    public void sumTwoNumbers(){
+        int result = Calculator.add("1,2");
+        assertEquals(3, result);
     }
 
 
