3d9ebe50310265bc2d7b3d841ff83ae8165ed3ef
Luigi Ferrarelli <luigi.ferrarelli@dodifferent.it>
2022-01-11 12:53:43 +0100

    test With One Number

--- java/src/test/java/com/xpeppers/CalculatorTest.java
+++ java/src/test/java/com/xpeppers/CalculatorTest.java
@@ -17,6 +17,11 @@ public class CalculatorTest {
         int result = Calculator.add("1,2");
         assertEquals(3, result);
     }
+    @Test
+    public void testWithOneNumber(){
+        int result = Calculator.add("1");
+        assertEquals(1, result);
+    }
 
 
 
