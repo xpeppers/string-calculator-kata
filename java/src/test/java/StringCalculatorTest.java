a8b86f141ed71b7bfadf486f7c4e622a9c3c38ff
emanuele.castagnaro <emanuele.castagnaro@arcavita.it>
2022-05-25 11:56:06 +0200

    inizio kata

---
+++ java/src/test/java/StringCalculatorTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StringCalculatorTest {
+    @Test
+    public void testprova(){
+        assertEquals(true,true);
+    }
+}
