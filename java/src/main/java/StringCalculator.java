314887a592b6a4984a666f7771286c5a7ce31d5e
emanuele.castagnaro <emanuele.castagnaro@arcavita.it>
2022-05-25 12:06:13 +0200

    step 1

--- java/src/main/java/StringCalculator.java
+++ java/src/main/java/StringCalculator.java
@@ -1,2 +1,12 @@
+import static java.lang.Integer.parseInt;
+
 public class StringCalculator {
+
+    public static final String EMPTY_STRING = "";
+
+    public int add(String input) {
+        if(input.equals(EMPTY_STRING))
+            return 0;
+        return parseInt(input);
+    }
 }
--- java/src/test/java/StringCalculatorTest.java
+++ java/src/test/java/StringCalculatorTest.java
@@ -3,8 +3,23 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class StringCalculatorTest {
+    private final StringCalculator stringCalculator=new StringCalculator();
+    /*    Step 1: the simplest thing
+    Create a simple String calculator with a method int add(String numbers).
+
+    The string argument can contain 0, 1 or 2 numbers, and will return their sum (for an empty string it will return 0) for example "" or "1" or "1,2".
+    Start with the simplest test case of an empty string and move to 1 and two numbers.
+    Remember to solve things as simply as possible so that you force yourself to write tests you did not think about.
+    Remember to refactor after each passing test.*/
+
     @Test
-    public void testprova(){
-        assertEquals(true,true);
+    public void empty_string_return_0(){
+        assertEquals(0, stringCalculator.add(""));
     }
+
+    @Test
+    public void string_with_one_number_return_number(){
+        assertEquals(1, stringCalculator.add("1"));
+    }
+
 }
