f555486e0f7d0cab0868942b36d505cf5a5dc427
imad.mouaouia <imad.mouaouia@finconsgroup.com>
2022-05-25 12:09:15 +0200

    step2

--- java/src/main/java/com/xpeppers/StringCalculator.java
+++ java/src/main/java/com/xpeppers/StringCalculator.java
@@ -2,14 +2,25 @@ package com.xpeppers;
 
 import java.util.Arrays;
 
+import static java.util.Arrays.*;
+
 public class StringCalculator {
+
+    public static final String COMMA = ",";
+    public static final String NEW_LINE = "\n";
+    public static final String EMPTY = "";
+
     public int add(String numbers) {
-        if (numbers.equals("")) {
+        if (numbers.equals(EMPTY)) {
             return 0;
         }
-        return Arrays.stream(numbers.split(","))
+        return stream(createArrayOfNumbers(numbers))
                 .map(Integer::parseInt)
                 .mapToInt(Integer::intValue)
                 .sum();
+    }
+
+    private String[] createArrayOfNumbers(String numbers) {
+        return numbers.replaceAll(NEW_LINE, COMMA).split(COMMA);
     }
 }
--- java/src/test/java/com/xpeppers/StringCalculatorTest.java
+++ java/src/test/java/com/xpeppers/StringCalculatorTest.java
@@ -5,10 +5,10 @@ import org.junit.Test;
 
 public class StringCalculatorTest {
 
+    private final StringCalculator stringCalculator = new StringCalculator();
+
     @Test
     public void should_return_0_when_empty_string() {
-        StringCalculator stringCalculator = new StringCalculator();
-
         int result = stringCalculator.add("");
 
         Assert.assertEquals(result, 0);
@@ -16,8 +16,6 @@ public class StringCalculatorTest {
 
     @Test
     public void should_return_1_when_input_is_1() {
-        StringCalculator stringCalculator = new StringCalculator();
-
         int result = stringCalculator.add("1");
 
         Assert.assertEquals(result, 1);
@@ -25,10 +23,20 @@ public class StringCalculatorTest {
 
     @Test
     public void should_return_3_when_input_is_1_comma_2() {
-        StringCalculator stringCalculator = new StringCalculator();
-
         int result = stringCalculator.add("1,2");
 
         Assert.assertEquals(result, 3);
+    }
+    @Test
+    public void should_return_sum_of_variabile_number_of_inputs() {
+        int result = stringCalculator.add("1,2,3,4");
+
+        Assert.assertEquals(result, 10);
+    }
+    @Test
+    public void should_return_sum_of_variabile_number_of_inputs_with_newLine_symbol() {
+        int result = stringCalculator.add("1\n2,3");
+
+        Assert.assertEquals(result, 6);
     }
 }
