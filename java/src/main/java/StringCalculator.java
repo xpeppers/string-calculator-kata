7cc417c24f5968ba53e897a9ab5508149a31492f
emanuele.castagnaro <emanuele.castagnaro@arcavita.it>
2022-06-14 11:29:07 +0200

    step 5,6 completati

--- java/src/main/java/StringCalculator.java
+++ java/src/main/java/StringCalculator.java
@@ -3,17 +3,44 @@ import static java.util.Arrays.stream;
 
 public class StringCalculator {
 
-    public static final String EMPTY_STRING = "";
+    private static final String EMPTY_STRING = "";
+    private String DELIMITER = ",";
+    private String negatives = EMPTY_STRING;
 
     public int add(String input) {
         if (input.equals(EMPTY_STRING))
             return 0;
-        String delimiter=",";
-        if(input.startsWith("//")) {
-            delimiter = input.substring(2, 3);
-            input=input.substring(4);
+        int sum = calculateSum(input);
+        if (negatives.isEmpty())
+            return sum;
+        throw new NumberFormatException("negatives not allowed:" + negatives);
+    }
+
+    private int calculateSum(String input) {
+        input = parse(input);
+        String[] lista = splitString(input.replace("\n", ","), DELIMITER);
+        return stream(lista)
+                .mapToInt(Integer::parseInt)
+                .filter(s -> s <= 1000)
+                .map(this::appendIfNegative)
+                .sum();
+    }
+
+    private int appendIfNegative(int s) {
+        if (s < 0)
+            negatives = negatives.concat(" " + s);
+        return s;
+    }
+
+    private String parse(String input) {
+        if (hasCustomDelimiter(input)) {
+            DELIMITER = input.substring(2, 3);
+            input = input.substring(4);
         }
-        String[] lista = splitString(input.replace("\n", ","), delimiter);
-        return stream(lista).mapToInt(Integer::parseInt).sum();
+        return input;
+    }
+
+    private boolean hasCustomDelimiter(String input) {
+        return input.startsWith("//");
     }
 }
--- java/src/test/java/StringCalculatorTest.java
+++ java/src/test/java/StringCalculatorTest.java
@@ -1,38 +1,63 @@
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
 
 public class StringCalculatorTest {
 
-    private final StringCalculator stringCalculator=new StringCalculator();
+    private final StringCalculator stringCalculator = new StringCalculator();
 
     @Test
-    public void empty_string_return_0(){
+    public void empty_string_return_0() {
         assertEquals(0, stringCalculator.add(""));
     }
 
     @Test
-    public void string_with_one_number_return_number(){
+    public void string_with_one_number_return_number() {
         assertEquals(1, stringCalculator.add("1"));
     }
 
     @Test
-    public void string_with_2_parameters_return_sum_of_them(){
+    public void string_with_2_parameters_return_sum_of_them() {
         assertEquals(3, stringCalculator.add("1,2"));
     }
 
     @Test
-    public void string_with_3_parameters_return_sum_of_them(){
+    public void string_with_3_parameters_return_sum_of_them() {
         assertEquals(6, stringCalculator.add("1,2,3"));
     }
 
     @Test
-    public void string_with_new_line_as_separator(){
+    public void string_with_new_line_as_separator() {
         assertEquals(6, stringCalculator.add("1\n2,3"));
     }
 
     @Test
-    public void xxx(){
+    public void custom_delimiter() {
         assertEquals(3, stringCalculator.add("//;\n1;2"));
+    }
+
+    @Test
+    public void should_throw_exception_when_negatives_in_input() {
+        String input = "//;\n-1;1;2";
+        NumberFormatException numberFormatException = assertThrows(NumberFormatException.class, () -> stringCalculator.add(input));
+        assertEquals("negatives not allowed: -1", numberFormatException.getMessage());
+    }
+
+    @Test
+    public void should_throw_exception_when_there_many_negatives_in_input() {
+        String input = "//;\n-1;-3;1;-2";
+        NumberFormatException numberFormatException = assertThrows(NumberFormatException.class, () -> stringCalculator.add(input));
+        assertEquals("negatives not allowed: -1 -3 -2", numberFormatException.getMessage());
+    }
+
+    @Test
+    public void should_ignore_big_number() {
+        assertEquals(2, stringCalculator.add("//;\n1001;2"));
+    }
+
+    @Test
+    public void should_not_ignore_1000() {
+        assertEquals(1002, stringCalculator.add("//;\n1000;2"));
     }
 }
